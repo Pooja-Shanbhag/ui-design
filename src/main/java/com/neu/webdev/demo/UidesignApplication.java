35fc724a87457a4c1c208f8663788db07bb2ce76
poojashanbhag <pooja.shanbhag1993@gmail.com>
2020-05-09 15:14:31 -0400

    Initial commit

---
+++ src/main/java/com/neu/webdev/demo/UidesignApplication.java
@@ -0,0 +1,13 @@
+package com.neu.webdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UidesignApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UidesignApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/neu/webdev/demo/UidesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.webdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UidesignApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
